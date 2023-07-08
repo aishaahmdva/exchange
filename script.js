f5225ca0a2d51c25b3388324db05bcb063f7e796
aisha <axmdvaisha@gmail.com>
2023-07-08 12:52:10 +0400

    change

---
+++ script.js
@@ -0,0 +1,15 @@
+let aze = prompt ('Dəyişmək istədiyiniz məbləği yazın');
+let usd = aze /  1.7;
+let euro = aze /  1.85;
+let tl = aze / 0.06523
+let rus = aze / 0.1861
+
+document.getElementById('aise').innerHTML = aze + ' m = ' + usd.toFixed() + ' $ ' ;
+
+document.getElementById('a').innerHTML = aze + ' m = ' + euro.toFixed() + ' € ' ;
+
+document.getElementById('i').innerHTML = aze + ' m = ' + tl.toFixed() + ' 	₺ ' ;
+
+document.getElementById('s').innerHTML = aze + ' m = ' + rus.toFixed() + ' 	₽ ' ;
+
+
